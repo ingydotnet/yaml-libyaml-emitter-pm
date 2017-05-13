33629925cf7a106759e1e7bc7d69b91cc21d1251
Ingy döt Net <ingy@ingy.net>
2017-05-14 00:19:04 +0200

    Got basic test working.

--- lib/YAML/LibYAML/Emitter.pm
+++ lib/YAML/LibYAML/Emitter.pm
@@ -2,7 +2,12 @@ use strict; use warnings;
 package YAML::LibYAML::Emitter;
 # our $VERSION = 'x.x.x';
 
-use YAML::LibYAML::Emitter::Inline C => 'lib/YAML/LibYAML/Emitter.c';
+use Cwd;
+
+use YAML::LibYAML::Emitter::Inline C
+    => 'lib/YAML/LibYAML/Emitter.c'
+    => inc => "-I${\ cwd()}/libyaml/include"
+    => myextlib => "${\ cwd()}/libyaml/src/.libs/libyaml.so";
 
 1;
 
